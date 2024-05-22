ebf2d82980203bf6e34379e86b1a937688bac104
Leandro Abreu Ferreira <leandro.abreu@elaw.com.br>
2024-05-21 22:51:45 -0300

    ci: ajust test coverage

--- sum.go
+++ sum.go
@@ -9,19 +9,3 @@ func main() {
 func sum(a int, b int) int {
 	return a + b
 }
-
-func sub(a int, b int) int {
-	return a - b
-}
-
-func times(a int, b int) int {
-	return a * b
-}
-
-func sumX(a int, b int) int {
-	return a + b + a
-}
-
-func timesSumX(a int, b int) int {
-	return a*b + a
-}
